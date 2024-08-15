8e6b9c927a516af10471581142b994f9288eddc2
Gihan sanjula <sanjulagihan94@gmail.com>
2024-08-15 23:54:08 +0530

    form edit

--- userLogin.php
+++ userLogin.php
@@ -4,28 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="abc.css">
     <title>Login Page</title>
 
 </head>
 <body>
     <?php include 'header.php'; ?>
-<div class="container-fluid mt-5 pt-5">
-<form>
+<div class="container-fluid mt-5 pt-5 d-flex justify-content-center align-items-center vh-100">
+<form class="mx-auto col-12 col-sm-8 col-md-6 col-lg-4 p-5">
+    <h3 class="text-center pb-3">Login</h3>
 
-  <div class="mb-3">
-    <input type="email" class="form-control" id="email1" aria-describedby="emailHelp" placeholder="email">
-  </div>
+    <div class="row mb-3">
+        <div class="col-md-12"> 
+        <input type="email" class="form-control" id="email1" aria-describedby="emailHelp" placeholder="email">
+        </div>
+    </div>
+
+  
 
   <div class="mb-3">
     <input type="password" class="form-control" id="password1" placeholder="password">
+    <div id="forgrt" class="form-text">Forget email?</div>
   </div>
 
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="check">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-
-  <button type="submit" class="btn btn-primary">Submit</button>
+ <div class="d-flex justify-content-center"><button type="submit" class="btn-primary"><span style="color: white;">Login</span></button></div>
   
 </form>
 </div>
