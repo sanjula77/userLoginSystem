180844c39e67805beb3300ea8a3579c511284745
Gihan sanjula <sanjulagihan94@gmail.com>
2024-08-18 01:10:13 +0530

    form edit

--- userLogin.php
+++ userLogin.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="aaa.css">
+    <link rel="stylesheet" href="rrr.css">
     <title>Login Page</title>
 
 </head>
@@ -32,7 +32,15 @@
     <hr class="text-secondary divider">
     <div class="divide-content-center">or</div>
 </div>
-
+<div class="text-center pt-2"><p>Continue with</p></div>
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-auto image-wrapper">
+                <a href="#"><img class="imag" src="icons8-google.svg" alt="Google"></a>
+                <a href="#"><img class="aple" src="logos--apple.svg" alt="Apple"></a>
+            </div>
+        </div>
+    </div>
 
 </form>
 </div>
--- validation.js
+++ validation.js
@@ -1,7 +1,11 @@
 function validate(){
     let email = document.getElementById("email1").value;
+    let password = document.getElementById("password1").value;
+    
     if(email == null || email == ""){
         alert('Please enter your email');
         return false;
+    }if(password == null || password == ""){
+        alert('Please enter password');
     }
 }
