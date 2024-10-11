e1adeada38ecbf42dd66705b5d4689c66223332a
Gihan sanjula <sanjulagihan94@gmail.com>
2024-10-12 00:08:09 +0530

    edit

--- login.php
+++ login.php
@@ -10,26 +10,25 @@ class login extends Dbh {
 
         if ($user) {
             if (password_verify($password, $user['password'])) {
-                // User is authenticated. You can start a session or perform any other actions.
                 session_start();
                 $_SESSION['user_id'] = $user['id'];
                 $_SESSION['user_email'] = $user['email'];
                 $_SESSION['user_name'] = $user['username'];
 
-                header("Location: dashboard.php"); // Redirect to the dashboard page
+                header("Location: dashboard.php"); 
                 exit();
             } else {
-                // Invalid password
+                
                 echo "Invalid password.";
             }
         } else {
-            // User not found
+            
             echo "User not found.";
         }
     }
 }
 
-// Usage example
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $password = $_POST['pass'];
